67c06936be80f3ebfc070dfe5c9310a3c3902522
Tariq Omar <tariqomar2004@gmail.com>
2025-09-17 12:50:24 +0300

    feature 1

---
+++ Implementations/Feature1/Controllers/CustomerController.cs
@@ -0,0 +1,32 @@
+﻿using Application.DTOs.CustomerModels;
+using Domain.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+
+[ApiController]
+[Route("api/amenity")]
+public class CustomerController : ControllerBase
+{
+    private readonly ICustomerService _customerService;
+
+    public CustomerController(ICustomerService customerService)
+    {
+        _customerService = customerService ?? throw new ArgumentNullException(nameof(customerService));
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterDto dto)
+    {
+        var customerId = await _customerService.RegisterAsync(dto);
+        return CreatedAtAction(nameof(Register), new { id = customerId }, new { Id = customerId });
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginDto dto)
+    {
+        var result = await _customerService.LoginAsync(dto);
+        return Ok(result);
+    }
+}
